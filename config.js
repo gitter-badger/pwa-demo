1cbb95e22f09c407b94d2df85300ed5a5227ed1a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2017-08-30 17:01:34 +0900

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyC57SCWkZ_uh_AyU2xUD0BhyIeG7fX7T-0'
+  gcmAPIKey: 'AAAAppG6WZE:APA91bFU9mREFTXd9OjiZsGOs7KkIimb1qxke7-oGNF59DkDzrW6c9BCm5tAE2UaPzRhnXojGRCO4OPq8uVxZlIfD86Ua6MjzAWVfUEIQ5VjpJeOtsJxJq9mQZa2gGQK-BVG5-8mNjeZ'
 };
